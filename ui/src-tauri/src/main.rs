9afdc34fab54c8299c56c761ee90dbbc07d7381b
theblind <1043193478@qq.com>
2022-11-30 20:23:33 +0800

    更新: 优化main.rs

--- ui/src-tauri/src/main.rs
+++ ui/src-tauri/src/main.rs
@@ -16,23 +16,6 @@ use tauri::api::process::{Command as tauriCommand, CommandEvent};
 fn main() {
 	let mut child = StdCommand::new("server.exe").creation_flags(0x08000000)
 		.spawn().expect("cmd exec error!");
-
-
-/*	let ( rx, child) = tauriCommand::new_sidecar("server")
-		.expect("failed to create `my-sidecar` binary command")
-		.spawn()
-		.expect("Failed to spawn sidecar");
-	let mut someChild=Some(child);*/
-
-
-/*	let mut command = StdCommand::from(
-		tauriCommand::new_sidecar("server")
-			.expect("Failed to create `backend_server` binary command").spawn()
-	);
-	let child = command.group_spawn().expect("Failed to spawn backend sidecar");*/
-
-
-
 	tauri::Builder::default()
 		.plugin(tauri_plugin_single_instance::init(|app, _argv, _cwd| { //设置插件
 			let window = app.get_window("main").unwrap(); //二次打开软件时，显示已打开窗口，单例运行app
@@ -46,14 +29,10 @@ fn main() {
 				println!("程序启动")
 			}
 			tauri::RunEvent::Exit => {
-
-				//child.kill().expect("!kill");
-				//let client = reqwest::Client::new();
 				let client = reqwest::blocking::Client::new();
 				let res = client.post("http://127.0.0.1:10218/system/shutdown")
 					.send();
 				println!("{:?}", res);
-				//someChild.take().unwrap().kill().expect("!kill");
 				child.kill().expect("!kill");
 				println!("程序退出")
 			}
