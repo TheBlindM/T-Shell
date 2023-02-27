b9c6db56ae920f6f6be34bc55b58b49e713385eb
theblind <1043193478@qq.com>
2023-02-27 12:25:32 +0800

    更新: doChat中请求方式为 java原生HttpClient

--- server/src/main/java/com/tshell/service/AiServer.java
+++ server/src/main/java/com/tshell/service/AiServer.java
@@ -1,16 +1,19 @@
 package com.tshell.service;
 
 import cn.hutool.core.util.StrUtil;
-import cn.hutool.http.HttpRequest;
-import cn.hutool.http.HttpResponse;
 import cn.hutool.http.HttpStatus;
-import cn.hutool.http.HttpUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import lombok.Data;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,38 +27,45 @@ public class AiServer {
     @Inject
     ObjectMapper objectMapper;
 
-    final  String openAiKey="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";
+    final String openAiKey = "密钥";
 
 
     public String doChat(String question) {
 
-        HttpRequest request = HttpUtil
-                .createPost("https://api.openai.com/v1/completions")
-                .header("Authorization","Bearer %s".formatted(openAiKey))
-                .header("Content-Type","application/json")
-                .timeout(1000*60);
+        String paramJson = "{\"model\": \"text-davinci-003\", \"prompt\": \"" + question.strip() + "\", \"temperature\": 0, \"max_tokens\": 1000}";
 
-        String paramJson = "{\"model\": \"text-davinci-003\", \"prompt\": \"" + question.strip() + "\", \"temperature\": 0, \"max_tokens\": 500}";
-        request.body(paramJson);
 
-        try (HttpResponse httpResponse = request.execute()) {
-            if (httpResponse.getStatus() == HttpStatus.HTTP_OK) {
-                String body = httpResponse.body();
-                try {
-                    AIAnswer aiAnswer = objectMapper.readValue(body, AIAnswer.class);
-                    return   aiAnswer.getChoices().stream().map(Choices::getText)
-                            .collect(Collectors.joining(StrUtil.EMPTY));
-                } catch (JsonProcessingException e) {
-                    throw new RuntimeException(e);
-                }
-            }else {
-                throw new RuntimeException("Ai 异常 code:" + httpResponse.getStatus());
+        // 创建HttpClient实例
+        HttpClient httpClient = HttpClient.newHttpClient();
+
+        // 创建HttpPost请求对象
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.openai.com/v1/completions"))
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Bearer %s".formatted(openAiKey))
+                .POST(HttpRequest.BodyPublishers.ofString(paramJson))
+                .build();
+
+        // 发送HttpPost请求并获取响应
+        java.net.http.HttpResponse<String> response = null;
+        try {
+            response = httpClient.<String>send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == HttpStatus.HTTP_OK) {
+                String body = response.body();
+                AIAnswer aiAnswer = objectMapper.readValue(body, AIAnswer.class);
+                return aiAnswer.getChoices().stream().map(Choices::getText)
+                        .collect(Collectors.joining(StrUtil.EMPTY));
+            } else {
+                throw new RuntimeException("Ai 异常 code:" + response.statusCode());
             }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 
 
     @Data
+    @RegisterForReflection
     public static class AIAnswer {
         private String id;
 
@@ -70,6 +80,7 @@ public class AiServer {
 
 
     @Data
+    @RegisterForReflection
     public static class Choices {
         private String text;
 
